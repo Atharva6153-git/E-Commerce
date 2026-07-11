e45b357dc25464e24198d844a14c9769c369bfb4
Atharva Nitin Jadhav <atharvaj7822@gmail.com>
2026-07-11 11:05:59 +0530

    Install and configure CORS package for inventory and payment services

--- payment-service/src/server.js
+++ payment-service/src/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const paymentRoutes = require('./routes/payment.routes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
